3511043db4faa65dedeb6f9070277a41949cece1
kodunmi <53086177+kodunmi@users.noreply.github.com>
2024-04-05 05:28:32 +0100

    added building

--- routes/api/v1/user/index.php
+++ routes/api/v1/user/index.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\V1\User\BuildingController;
 use App\Http\Controllers\V1\User\ComplianceController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('compliance')->controller(ComplianceController::class)->group(function () {
     Route::post('/', 'completeCompliance');
+});
+
+Route::prefix('buildings')->controller(BuildingController::class)->group(function () {
+    Route::post('/', 'addBuilding');
+    Route::get('/', 'getBuildings');
+    Route::get('/{building_id}', 'getBuilding');
+    Route::delete('/', 'removeBuilding');
+
+    Route::prefix('public')->group(function () {
+        Route::get('/', 'getAllAvailableBuildings');
+    });
 });
